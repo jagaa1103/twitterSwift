ac573dd2974e82a4e3a0ec98e878ffeae46d1747
Jagaa <jagaa1103@gmail.com>
2016-02-04 13:45:22 +0100

    starting

---
+++ twitterSwift/twitterSwift/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  twitterSwift
+//
+//  Created by Enkhjargal Gansukh on 2/4/16.
+//  Copyright © 2016 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tweetText: UITextView!
+    @IBAction func showShareAction(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
